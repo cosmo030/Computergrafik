e5fc7eadeaa5ff0ddcf5d1748e392ad59d50ad8f
cosmo <eliasbraun60@gmail.com>
2025-10-31 15:42:04 +0100

    completed A3.1

--- src/app/app.java
+++ src/app/app.java
@@ -20,33 +20,36 @@ public class app {
     Raytracer rt = new Raytracer(cam, Color.black);
 
     // add spheres
-    rt.addSphere(new Sphere(new Vec3(1, -3, -35), 4, Color.beige));
-    rt.addSphere(new Sphere(new Vec3(0, 0, -20), 1, Color.blue));
-    rt.addSphere(new Sphere(new Vec3(-2.5, -1.5, -15), 1.5, Color.green));
-    rt.addSphere(new Sphere(new Vec3(-3, 0, -15), 2, Color.magenta));
-    rt.addSphere(new Sphere(new Vec3(5, 0, -40), 4, Color.cyan));
-    rt.addSphere(new Sphere(new Vec3(8, 2, -40), 3, Color.yellow));    
-    rt.addSphere(new Sphere(new Vec3(-1.8, 1.3, -10), 0.7, Color.gray));    
-    rt.addSphere(new Sphere(new Vec3(-1.8, 2, -21), 1.5, Color.white));    
+    rt.addSphere(new SphereShape(new Vec3(1, -3, -35), 4, Color.beige));
+    rt.addSphere(new SphereShape(new Vec3(0, 0, -20), 1, Color.blue));
+    rt.addSphere(new SphereShape(new Vec3(-2.5, -1.5, -15), 1.5, Color.green));
+    rt.addSphere(new SphereShape(new Vec3(-3, 0, -15), 2, Color.magenta));
+    rt.addSphere(new SphereShape(new Vec3(5, 0, -40), 4, Color.cyan));
+    rt.addSphere(new SphereShape(new Vec3(8, 2, -40), 3, Color.yellow));
+    rt.addSphere(new SphereShape(new Vec3(-1.8, 1.3, -10), 0.7, Color.gray));
+    rt.addSphere(new SphereShape(new Vec3(-1.8, 2, -21), 1.5, Color.white));
 
     // iterate over all pixel of the image
     var image = new Image(width, height);
     for (int i = 0; i != width; i++) {
-      for (int j = 0; j != height; j++) {      
-        /* Ray ray = cam.generate_ray(new Vec2(i, j));
-        Vec3 d = ray.getDValue();
-        Vec3 n = Vec3.normalize(d);
-        double r = n.x();
-        double g = n.y();
-        double b = 0.0;
-        Color color = new Color(r, g, b); */
-        image.setPixel(i, j, rt.getColor(new Vec2(i,j)));
+      for (int j = 0; j != height; j++) {
+        /*
+         * Ray ray = cam.generate_ray(new Vec2(i, j));
+         * Vec3 d = ray.getDValue();
+         * Vec3 n = Vec3.normalize(d);
+         * double r = n.x();
+         * double g = n.y();
+         * double b = 0.0;
+         * Color color = new Color(r, g, b);
+         */
+        image.setPixel(i, j, rt.getColor(new Vec2(i, j)));
       }
     }
 
     // Write the image to disk.
     image.writePNG("a02_spheres");
 
-    IntersectionTest.runAll();;
+    IntersectionTest.runAll();
+    ;
   }
 }
