2d98cecf133e3e057e969db54fb19c74855dc03e
cosmo <eliasbraun60@gmail.com>
2025-11-02 14:58:34 +0100

    completed A3.5 own test png

--- src/app/app.java
+++ src/app/app.java
@@ -21,17 +21,20 @@ public class app {
     Camera cam = new Camera(width, height, 70.0);
     GroupShape scene = new GroupShape();
 
-    DiscShape ds = new DiscShape(new Vec3(0, -0.5, 0), 100, Color.yellow);
-    BackgroundShape bs = new BackgroundShape(Color.multiply(0.7, Color.white));
-    scene.add(new SphereShape(new Vec3(-1, -0.25, -2.5), .7, Color.red));
-    scene.add(new SphereShape(new Vec3(0, -0.25, -2.5), .5, Color.green));
-    scene.add(new SphereShape(new Vec3(1, -0.25, -2.5), .7, Color.blue));
+    DiscShape ds = new DiscShape(new Vec3(0, -0.5, 0), 100, Color.beige);
+    BackgroundShape bs = new BackgroundShape(Color.multiply(0.7, Color.cyan));
+    scene.add(new SphereShape(new Vec3(-2.3, -.25, -3.5), .3, Color.red));
+    scene.add(new SphereShape(new Vec3(-1.5, .0, -3.0), .4, Color.green));
+    scene.add(new SphereShape(new Vec3(-.8, .0, -3.0), .45, Color.blue));
+    scene.add(new SphereShape(new Vec3(-.2, -.25, -2.5), .5, Color.red));
+    scene.add(new SphereShape(new Vec3(.5, -.5, -2.0), .6, Color.green));
+    scene.add(new SphereShape(new Vec3(1.1, -.75, -1.5), .5, Color.blue));
     scene.add(ds);
     scene.add(bs);
 
     Raytracer raytracer = new Raytracer(cam, scene, Color.black);
     image.sample(raytracer);
     // Write the image to disk.
-    image.writePNG("a03-test-scene");
+    image.writePNG("a03-own-scene");
   }
 }
